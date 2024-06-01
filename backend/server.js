b9e813d85bd6df26e718e525a99e6340a1f334a2
Aron Bahram <aron.bahram@gmail.com>
2024-06-02 01:05:38 +0200

    feat: test node database connection

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { Sequelize } = require('sequelize');
+const cors = require('cors');
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Initialize Sequelize
+const sequelize = new Sequelize({
+    dialect: 'postgres',
+    database: 'fitboost',
+    user: 'baron',
+    host: '/var/run/postgresql',
+    port: 5432,
+    ssl: true,
+    clientMinMessages: 'notice',
+});
+
+// Test the database connection
+sequelize.authenticate()
+  .then(() => console.log('Database connected'))
+  .catch(err => console.error('Unable to connect to the database:', err));
+
+app.get('/', (_req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
